3ba0f90f09236cd61194a68081aca081cd1bd505
Steve Gravrock <sgravrock@pivotal.io>
2016-11-01 10:22:30 -0700

    Add new tasks

--- jasmine/spec/todoControllerSpec.js
+++ jasmine/spec/todoControllerSpec.js
@@ -3,9 +3,29 @@ describe("TodoController", function() {
     this.dom = document.createElement("div");
     this.subject = new TodoMvc.TodoController();
     this.subject.appendTo(this.dom);
+    this.textField = this.dom.querySelector("input.new-task-name");
+    this.addButton = this.dom.querySelector(".add-task");
   });
 
-  it("says hello", function() {
-    expect(this.dom.textContent).toEqual("Hello, world!");
+  it("shows a text entry box", function() {
+    expect(this.textField).toBeTruthy();
+    expect(this.textField.placeholder).toEqual("new task");
+  });
+
+  describe("When the user enters a name and clicks Add", function() {
+    beforeEach(function() {
+      this.textField.value = "New Task 1";
+      this.addButton.click();
+    });
+
+    it("shows the new task", function() {
+      var item = this.dom.querySelector("li");
+      expect(item).toBeTruthy();
+      expect(item.textContent).toEqual("New Task 1");
+    });
+
+    it("clears the text field", function() {
+      expect(this.textField.value).toBeFalsy();
+    });
   });
 });
--- todoController.js
+++ todoController.js
@@ -1,13 +1,33 @@
 (function() {
   "use strict";
+  var template =
+    '<input type="text" class="new-task-name" placeholder="new task">' +
+    '<button class="add-task">Add</button>' +
+    '<ol></ol>';
+
   function TodoController() {
+    var that = this;
     this._dom = document.createElement("div");
     this._dom.className = "todo-controller";
-    this._dom.textContent = "Hello, world!";
+    this._dom.innerHTML = template;
+    this._newTaskField = this._dom.querySelector(".new-task-name");
+    this._list = this._dom.querySelector("ol");
+    this._dom.querySelector(".add-task").addEventListener("click",
+      function(event) {
+        event.preventDefault();
+        that._addClicked();
+      });
   };
 
   TodoController.prototype.appendTo = function(parent) {
     parent.appendChild(this._dom);
+  };
+
+  TodoController.prototype._addClicked = function() {
+    var li = document.createElement("li");
+    li.textContent = this._newTaskField.value;
+    this._list.appendChild(li);
+    this._newTaskField.value = "";
   };
 
   TodoMvc.TodoController = TodoController;
